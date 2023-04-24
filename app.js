6d79c76234edd2ac82df05fdb90c36d4bb7db1a0
Ignacio <ignacioCoriadb@gmail.com>
2023-04-24 16:33:18 -0300

    config mercadopago

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = require('./src/routes/index');
 const server = express();
+const mercadopago = require('mercadopago');
+
+mercadopago.configure({
+    access_token: process.env.KEY_MERCADOPAGO
+})
 
 server.use(express.json());
 server.use("/",router);
