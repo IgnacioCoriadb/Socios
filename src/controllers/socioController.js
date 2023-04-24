8046afdea639890ecd54b3874ac48dd917adb7b8
Ignacio <ignacioCoriadb@gmail.com>
2023-04-24 11:46:17 -0300

    getAllSocio, getSocioId

--- src/controllers/socioController.js
+++ src/controllers/socioController.js
@@ -1,9 +1,27 @@
-const getSocio= (req, res)=>{
-    console.log("Funciona")
-    res.json("Funciona");
+const {Socio} = require('../../database');
+
+//?---------------------------------ALLSOCIO---------------------------------------------------
+const getAllSocio=async (req, res)=>{
+    try{
+        const socios = await Socio.findAll();
+        res.json(socios);
+    }catch(error){
+        const errorMessage = '"An error occurred while connecting to the database." ' + error.message;
+        res.status(500).json({ error: errorMessage });
+    }
+}
+
+//?------------------------getSocioId---------------------------------------------------------  
+const getSocioId = async (req, res)=>{
+    const {idSocio} = req.params;
+    try{
+        const socio = await Socio.findByPk(idSocio)
+        res.json(socio)
+    }catch(error){
+        const errorMessage = "No exist member with id number " + idSocio;
+        res.status(500).json({ error: errorMessage });
+    }
 }
 
 
-
-
-module.exports ={getSocio}
+module.exports ={getAllSocio,getSocioId}
