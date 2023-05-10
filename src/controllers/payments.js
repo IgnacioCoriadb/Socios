0ef8f5f15f9d46533c8f71c539ade957603c0cce
Ignacio <ignacioCoriadb@gmail.com>
2023-05-10 16:33:04 -0300

    suscription and pay update DB

--- src/controllers/payments.js
+++ src/controllers/payments.js
@@ -44,109 +44,149 @@ const buttonPayment = (req, res) => {
   });
 }
 
-
-//?Respuesta a el pago con boton:
-    //?buscar en la bd la fecha del ultimo pago (lastPayment)
-    //?cambiar la fecha a el siguiente mes, ejemplo: si pago abril cambiar a mayo 
-    //?si el mes de last payment es el mes actual cambiar status a true, esto quiere decir que el socio esta al día 
-
 const response =async (req,res)=>{
+  const {type} = req.body;
+  if(type === 'payment'){
+    responsePayment(req, res);
+  }
+  if(type === 'subscription_preapproval'){
+    responesSubscription(req, res);
+  }
+}
 
+const subscription =async (req,res)=>{
+
+  const subscriptionData = {
+    reason: "Colaboración mensual",
+    payer_email: 'test_user_1302851605@testuser.com',
+    external_reference:User1.id,
+    auto_recurring: {
+      frequency: 1,
+      frequency_type: 'months',
+      transaction_amount: 22,
+      currency_id: 'ARS',
+      // start_date: new Date(),
+      // end_date: new Date().setFullYear(new Date().getFullYear() + 1)
+    },
+    back_url: 'https://www.tu-sitio.com/feedback',
+
+
+  };
+  
+    const result = await mercadopago.preapproval.create(subscriptionData);
+    res.json({id:result.body.id, link: result.body.init_point})
+
+}
+
+const responsePayment = async(req, res)=>{
   const payment = await mercadopago.payment.findById(req.body.data.id);//id del pagador
   const merchanOrder = await mercadopago.merchant_orders.findById(payment.body.order.id); //pago 
   const idUser =payment.body.metadata.id_user; //id de user pagador
   const paymentLastDateHs = merchanOrder.response.last_updated; //fecha mas hora
   const paymentDate = paymentLastDateHs.slice(0,10); //fecha sin hora
-  const paymentLastMonth = await Socio.findByPk(idUser,{ attributes: ['month', 'year']});
 
   merchanOrder.body.payments.forEach(async (payment) => {
-    // console.log(payment)
     if(payment.status === 'approved' && payment.status_detail ==='accredited'){
       console.log('Pago exitoso');
-
-      //update db lastpayment
-      await Socio.update(
-        {
-          lastPayment: paymentDate
-        },
-        {
-          where: {
-            id: idUser
-          }
-        }
-      );
-
-    //le sumo uno a el ultimo mes pago si son mas de doce meses vuelve a uno y cambia el año 
-    if(paymentLastMonth.month < 12){
-      const newMonth  =paymentLastMonth.month +1;
-      await Socio.update(
-        {
-          month:newMonth
-        },
-        {
-          where: {
-            id: idUser
-          }
-        }
-      )
-    }else{
-      const newYear = paymentLastMonth.year +1;
-      await Socio.update(
-        {
-          month: 1,
-          year: newYear
-        },
-        {
-          where: {
-            id: idUser
-          }
-        }
-      )
-    }
-
-      //si el ultimo mes pago es el mes actual cambiar status a true
-      const currentDate = new Date();
-      const currentMonth = currentDate.getMonth() + 1; // suma uno porque toma de 0 a 11
-      const currentYear = currentDate.getFullYear();
-      //vuelvo a consultar día y fecha despues de actualizar 
-      const paymentLastMonthUpdated = await Socio.findByPk(idUser,{ attributes: ['month', 'year']});
-
-      //si coinicide el socio está al día
-      if(paymentLastMonthUpdated.month >=  currentMonth && paymentLastMonthUpdated.year >= currentYear){
-        console.log('Cuota al día')
-        await Socio.update(
-          {
-            status: true
-          },
-          {
-            where:{
-              id: idUser
-            }
-          }
-        )
-      }else{
-        await Socio.update(
-          {
-            status: false
-          },
-          {
-            where:{
-              id: idUser
-            }
-          }
-        )
-        console.log(`El ultimo mes pago es ${paymentLastMonth.month}/${paymentLastMonth.year}`)
-      }
+      updateDbSocios(paymentDate,idUser)
+      
     }else{
         console.log('No se pudo realizar el pago ')
     }
-  });
+  });  
 
-res.status(200).json("OK")
+  res.status(200).json("OK")
 }
 
+const responesSubscription = async (req, res) => {
+  const payment= await  mercadopago.preapproval.findById(req.body.data.id, response=> response);
+  const idUser = payment.body.external_reference;
+    if(payment.response.status === 'authorized'){
+      const paymentLastDateHs = payment.response.last_modified; //fecha y hora del pago 
+      const paymentDate = paymentLastDateHs.slice(0,10); //fecha sin hora
+      updateDbSocios(paymentDate,idUser)
+    }else{
+      console.log('Pending')
+    }
+    res.status(200).json("OK")
+}
+
+const updateDbSocios =async (paymentDate,idUser)=>{
+  const paymentLastMonth = await Socio.findByPk(idUser,{ attributes: ['month', 'year']});
+  //update db lastpayment
+  await Socio.update(
+    {
+      lastPayment: paymentDate
+    },
+    {
+      where: {
+        id: idUser
+      }
+    }
+  );
+  //le sumo uno a el ultimo mes pago si son mas de doce meses vuelve a uno y cambia el año 
+  if(paymentLastMonth.month < 12){
+    const newMonth  =paymentLastMonth.month +1;
+    await Socio.update(
+      {
+        month:newMonth
+      },
+      {
+        where: {
+          id: idUser
+        }
+      }
+    )
+  }else{
+    const newYear = paymentLastMonth.year +1;
+    await Socio.update(
+      {
+        month: 1,
+        year: newYear
+      },
+      {
+        where: {
+          id: idUser
+        }
+      }
+    )
+  }
+  //si el ultimo mes pago es el mes actual cambiar status a true
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth() + 1; // suma uno porque toma de 0 a 11
+  const currentYear = currentDate.getFullYear();
+  //vuelvo a consultar día y fecha despues de actualizar 
+  const paymentLastMonthUpdated = await Socio.findByPk(idUser,{ attributes: ['month', 'year']});
+  //si coinicide el socio está al día
+  if(paymentLastMonthUpdated.month >=  currentMonth && paymentLastMonthUpdated.year >= currentYear){
+    console.log(`Cuota al día, tiene pago hasta el mes ${paymentLastMonthUpdated.month} del ${paymentLastMonthUpdated.year}`)
+    await Socio.update(
+      {
+        status: true
+      },
+      {
+        where:{
+          id: idUser
+        }
+      }
+    )
+  }else{
+    await Socio.update(
+      {
+        status: false
+      },
+      {
+        where:{
+          id: idUser
+        }
+      }
+    )
+    console.log(`El ultimo mes pago es ${paymentLastMonth.month}/${paymentLastMonth.year}`)
+  }
+}
 
 module.exports ={
     buttonPayment,
-    response
+    response,
+    subscription
 }
--- src/routes/payments.js
+++ src/routes/payments.js
@@ -1,10 +1,11 @@
 const Router = require('express');
 const payments = Router();
-const {buttonPayment, response} = require('../controllers/payments');
+const {buttonPayment, response,subscription} = require('../controllers/payments');
 
 
 payments.post("/",buttonPayment);
 payments.post("/response", response);
+payments.post("/subscription", subscription);
 
 
 module.exports = payments;
